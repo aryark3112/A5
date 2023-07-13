64ffa4a6a8c5d06a53080eaa1d68fd633d77c8ac
aryark <aryark3112@gmail.com>
2023-07-13 15:28:46 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace july13A5
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+
+
+//            }
+//    }
+//}
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ValiduserName = "john";
+            string Validpassword = "pass123";
+
+
+
+            int attempts = 3;
+            while (attempts > 0)
+            {
+                Console.WriteLine("Enter Username:\t");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Enter Password");
+                string password = Console.ReadLine();
+
+                if (userName == ValiduserName && password == Validpassword)
+                {
+                    Console.WriteLine("Login Successfull!");
+                    break;
+
+                }
+
+                else if (userName == ValiduserName)
+                {
+                    Console.WriteLine("Enter the valid Password");
+                    attempts--;
+                    Console.WriteLine($"You have {attempts} remaining attempts");
+                }
+                else
+                {
+                    Console.WriteLine("Enter the Valid Username");
+                    attempts--;
+                    Console.WriteLine($"You have {attempts} remaining attempts");
+
+                }
+
+                if (attempts == 0)
+                {
+                    Console.WriteLine("Login Failed");
+
+                }
+
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
